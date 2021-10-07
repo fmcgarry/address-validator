1e7b003d33dcb416d8ce6164c701e18d913b53b8
fmcgarry <freddymcgarry3@gmail.com>
2021-10-07 17:15:28 -0400

    added tests project

---
+++ tests/ValidationAPITests/ValidAddressTests.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ValidationAPITests
+{
+	[TestClass]
+	public class ValidAddressTests
+	{
+		[TestMethod]
+		public void IsValidAddress_ReturnsTrue()
+		{
+		}
+	}
+}
