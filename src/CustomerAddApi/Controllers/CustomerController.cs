bef362e92c4412fd03fc0d7d432f2ece288207e1
fmcgarry <freddymcgarry3@gmail.com>
2021-10-09 00:23:53 -0400

    now upserting customer into crm

--- src/CustomerAddApi/Controllers/CustomerController.cs
+++ src/CustomerAddApi/Controllers/CustomerController.cs
@@ -4,6 +4,7 @@ using Microsoft.Extensions.Logging;
 using AddressValidation;
 using AddressValidation.Web.Models;
 using AddressValidation.Web.Converters;
+using AddressValidator.Infrastructure;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -14,12 +15,14 @@ namespace CustomerAddApi.Controllers
 	public class CustomerController : ControllerBase
 	{
 		private readonly UspsAddressValidator addressValidator;
+		private readonly ICrmRepository crm;
 		private readonly ILogger<CustomerController> logger;
 
-		public CustomerController(ILogger<CustomerController> logger, UspsAddressValidator addressValidator)
+		public CustomerController(ILogger<CustomerController> logger, UspsAddressValidator addressValidator, ICrmRepository crm)
 		{
 			this.logger = logger;
 			this.addressValidator = addressValidator;
+			this.crm = crm;
 		}
 
 		[HttpPost]
@@ -30,6 +33,7 @@ namespace CustomerAddApi.Controllers
 			if (coreCustomer is not null)
 			{
 				await addressValidator.ValidateCustomerAddressAsync(coreCustomer);
+				await crm.UpsertCustomer(coreCustomer);
 
 				// should actually return 201 status code, but no Get method is implemented.
 				// return CreatedAtAction(nameof(GetById), new { id = customer.Id }, customer);
--- src/CustomerAddApi/Startup.cs
+++ src/CustomerAddApi/Startup.cs
@@ -1,3 +1,4 @@
+using AddressValidator.Infrastructure;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
@@ -47,6 +48,7 @@ namespace CustomerAddApi
 				c.SwaggerDoc("v1", new OpenApiInfo { Title = "CustomerAddApi", Version = "v1" });
 			});
 			services.AddSingleton<AddressValidation.UspsAddressValidator>();
+			services.AddTransient<ICrmRepository, CrmRepository>();
 		}
 	}
 }
